ab25caec22efe2a3b3ad5581787a7de06a5ce7da
Kristen M. Thyng <kthyng@gmail.com>
2014-01-10 15:56:05 -0600

    preserving state of notebook, with s, all old calcs, and cloud statistics

--- run_calcs.py
+++ run_calcs.py
@@ -6,36 +6,47 @@ import netCDF4 as netCDF
 import numpy as np
 import pdb
 
-locsave = 'calcs/' #test/'# '/Volumes/Emmons/projects/time_res/'
+locsave = 'calcs/' #'calcs/4th/' #test/'# '/Volumes/Emmons/projects/time_res/'
 
 # Loop through tracks files and run calculations
-floc = '/Volumes/Emmons/projects/time_res/tracks/' #test/'
+floc = '/Volumes/Emmons/projects/time_res/tracks/' #4th/' #test/'
 Files = glob.glob(floc + '*.nc')
 # Files = glob.glob('tracks/*.nc')
+# dc = netCDF.Dataset('/Volumes/Emmons/projects/time_res/tracks/4th/tseas_use300_nsteps1.nc') # control case, 5 min output
 dc = netCDF.Dataset('/Volumes/Emmons/projects/time_res/tracks/tseas_use300_nsteps1.nc') # control case, 5 min output
 # dc = netCDF.Dataset('/Volumes/Emmons/projects/time_res/tracks/test/tseas_use300_nsteps1.nc') # control case, 5 min output
-# dc = netCDF.Dataset('tracks/tseas_use300_nsteps1.nc') # control case, 5 min output
+# # dc = netCDF.Dataset('tracks/tseas_use300_nsteps1.nc') # control case, 5 min output
 lonpc = dc.variables['lonp'][:]; latpc = dc.variables['latp'][:]; tpc = dc.variables['tp'][:];
 
 loc = ['/Volumes/Emmons/projects/time_res/crcm/', '/Volumes/Emmons/projects/time_res/forgrid/ocean_his_0703_2010-07-03_00.nc']
 # loc = ['crcm/', '/pong/raid/data/crcm/BP/2010/0703/ocean_his_0703_2010-07-03_00.nc']
-grid = tracpy.inout.readgrid(loc)
-
+llcrnrlon=-97.944923400878906; urcrnrlon=-79.164894104003906;
+llcrnrlat=18.132225036621094; urcrnrlat=30.847625732421861
+grid = tracpy.inout.readgrid(loc, llcrnrlon=llcrnrlon, urcrnrlon=urcrnrlon,
+                                  llcrnrlat=llcrnrlat, urcrnrlat=urcrnrlat)  # grid file, Gulf model domain
+# grid = tracpy.inout.readgrid(loc[1], vert_filename=loc[0], llcrnrlon=llcrnrlon, urcrnrlon=urcrnrlon,
+#                                   llcrnrlat=llcrnrlat, urcrnrlat=urcrnrlat)  # grid file, Gulf model domain
 nc = netCDF.MFDataset('/Volumes/Emmons/projects/time_res/crcm/ocean*.nc')
 dt = 1000 # number of time indices to loop through at once
-Files = [floc + 'tseas_use28800_nsteps96.nc', floc + 'tseas_use36000_nsteps120.nc']
+# Files = [floc + 'tseas_use28800_nsteps96.nc', floc + 'tseas_use36000_nsteps120.nc']
 # Files = [floc + 'tseas_use57600_nsteps192.nc', floc + 'tseas_use86400_nsteps288.nc']
 for File in Files:
 
     if 'gc' in File:# or '10800' in File:
         continue
 
-    # if '10800' not in File:
+    # if '2700' not in File:
     #     continue
 
+    # if '10800' not in File:
+    #     continue
+    print File
     # Read in tracks
     d = netCDF.Dataset(File)
     lonp = d.variables['lonp'][:]; latp = d.variables['latp'][:]; tp = d.variables['tp'][:];
+
+    # Project into x and y coords
+    xp, yp = grid['basemap'](lonp,latp)
 
     # # change tracks to grid coords
     # # tracpy.inout.save_ll2grid(floc, File.split('/')[-1], grid)
@@ -63,11 +74,11 @@ for File in Files:
     # np.savez(locsave + File.split('/')[-1][:-3] + 'Dcomp.npz', D2=D2, t=tp, 
     #                         nnans=nnans, imax=imax, imin=imin, D2max=D2max, D2min=D2min)
 
-    # Numerical D^2
-    D2, nnans, imax, imin, D2max, D2min = tracpy.calcs.rel_dispersion_comp(lonpc, latpc, 
-                                            tpc, lonp, latp, tp, r=1, squared=True)
-    np.savez(locsave + File.split('/')[-1][:-3] + 'D2comp.npz', D2=D2, t=tp, 
-                            nnans=nnans, imax=imax, imin=imin, D2max=D2max, D2min=D2min)
+    # # Numerical D^2
+    # D2, nnans, imax, imin, D2max, D2min = tracpy.calcs.rel_dispersion_comp(lonpc, latpc, 
+    #                                         tpc, lonp, latp, tp, r=1, squared=True)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'D2comp.npz', D2=D2, t=tp, 
+    #                         nnans=nnans, imax=imax, imin=imin, D2max=D2max, D2min=D2min)
 
     # # Physical D
     # D2, nnans = tracpy.calcs.rel_dispersion(lonp, latp, r=1, squared=False)
@@ -93,17 +104,17 @@ for File in Files:
     # D2, nnans = tracpy.calcs.path(lonp, latp, squared=True)
     # np.savez(locsave + 'calcs/' + File.split('/')[-1][:-3] + 's2.npz', D2=D2, t=tp, nnans=nnans)
 
-    # Numerical D/s
-    D2, nnans, imax, imin, D2max, D2min = tracpy.calcs.rel_dispersion_comp_ds(lonpc, latpc,
-                                             tpc, lonp, latp, tp, r=1, squared=False)
-    np.savez(locsave + File.split('/')[-1][:-3] + 'Dcompds.npz', D2=D2, t=tp, 
-                                nnans=nnans, imax=imax, imin=imin, D2max=D2max, D2min=D2min)
+    # # Numerical D/s
+    # D2, nnans, imax, imin, D2max, D2min = tracpy.calcs.rel_dispersion_comp_ds(lonpc, latpc,
+    #                                          tpc, lonp, latp, tp, r=1, squared=False)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'Dcompds.npz', D2=D2, t=tp, 
+    #                             nnans=nnans, imax=imax, imin=imin, D2max=D2max, D2min=D2min)
 
-    # Numerical D/a
-    D2, nnans, imax, imin, D2max, D2min = tracpy.calcs.rel_dispersion_comp_da(lonpc, latpc,
-                                             tpc, lonp, latp, tp, r=1, squared=False)
-    np.savez(locsave + File.split('/')[-1][:-3] + 'Dcompda.npz', D2=D2, t=tp, 
-                                nnans=nnans, imax=imax, imin=imin, D2max=D2max, D2min=D2min)
+    # # Numerical D/a
+    # D2, nnans, imax, imin, D2max, D2min = tracpy.calcs.rel_dispersion_comp_da(lonpc, latpc,
+    #                                          tpc, lonp, latp, tp, r=1, squared=False)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'Dcompda.npz', D2=D2, t=tp, 
+    #                             nnans=nnans, imax=imax, imin=imin, D2max=D2max, D2min=D2min)
 
     # # For a single drifter or a few drifters
     # # Numerical D/s
@@ -118,3 +129,47 @@ for File in Files:
     #                                          tpc, lonp[ind,:], latp[ind,:], tp, r=1, squared=False)
     # np.savez(locsave + 'calcs/' + File.split('/')[-1][:-3] + 'Dcompda_624_814.npz', D2=D2, t=tp, 
     #                             nnans=nnans, imax=imax, imin=imin, D2max=D2max, D2min=D2min)
+
+    # # 1st moments
+    # M1x, nnans = tracpy.calcs.moment1(xp)
+    # # np.savez(locsave + File.split('/')[-1][:-3] + 'M1x.npz', M=M1x, t=tp, nnans=nnans)
+    # M1y, nnans = tracpy.calcs.moment1(yp)
+    # # np.savez(locsave + File.split('/')[-1][:-3] + 'M1y.npz', M=M1y, t=tp, nnans=nnans)
+
+    # 2nd moments
+    # M2x, nnans = tracpy.calcs.moment2(xp, M1x)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'M2x.npz', M=M2x, t=tp, nnans=nnans)
+    # M2y, nnans = tracpy.calcs.moment2(yp, M1y)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'M2y.npz', M=M2y, t=tp, nnans=nnans)
+
+    # # 3rd moments
+    # M3x = tracpy.calcs.moment3(xp, M1x)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'M3x.npz', M=M3x, t=tp)
+    # M3y = tracpy.calcs.moment3(yp, M1y)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'M3y.npz', M=M3y, t=tp)
+ 
+    # # 4th moments
+    # M4x, nnans = tracpy.calcs.moment4(xp, M1x)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'M4x.npz', M=M4x, t=tp, nnans=nnans)
+    # M4y, nnans = tracpy.calcs.moment4(yp, M1y)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'M4y.npz', M=M4y, t=tp, nnans=nnans)
+
+    # # Trajectory skill score
+    # l = min((lonpc.shape[0], lonp.shape[0]))
+    # ss = tracpy.calcs.traj_ss(lonpc[:l,:], latpc[:l,:], lonp[:l,:], latp[:l,:])
+    # np.savez(locsave + File.split('/')[-1][:-3] + 's.npz', ss=ss, t=tp)
+    # # ssmean = ss.mean(axis=0)
+    # # np.savez(locsave + File.split('/')[-1][:-3] + 'ssmean.npz', ssmean=ssmean, t=tp)
+
+    # Change from s to ss and get means
+    n = 0.1; nname = 'n0p1'
+    d = np.load(locsave + File.split('/')[-1][:-3] + 's.npz')
+    s = d['ss']; t=d['t']
+    smean = s.mean(axis=0)
+    # ind = (s>n)
+    # ss = 1-s/n
+    # ss[ind] = 0.
+    # ssmean = ss.mean(axis=0)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'ss_' + nname + '.npz', ss=ss, t=t, n=n)
+    # np.savez(locsave + File.split('/')[-1][:-3] + 'ssmean_' + nname + '.npz', ssmean=ssmean, t=t, n=n)
+    np.savez(locsave + File.split('/')[-1][:-3] + 'smean_' + nname + '.npz', smean=smean, t=t)
